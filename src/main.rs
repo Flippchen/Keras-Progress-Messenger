f0b17b6898cc5f75c6e77bcc1a81dc44abe39aa2
Flippchen <91947480+Flippchen@users.noreply.github.com>
2023-03-27 10:31:38 +0200

    finalized server and added env variables

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,26 @@
+mod webhook;
+
+use std::env;
 use hyper::{Body, Request, Response, Server};
 use hyper::service::{make_service_fn, service_fn};
 use hyper::{Error, StatusCode};
 use serde_json::Value;
 use std::net::SocketAddr;
 
+use crate::webhook::send_discord_notification;
+
+
 async fn handle_request(req: Request<Body>) -> Result<Response<Body>, Error> {
     // Read the body and parse it as JSON
     let body_bytes = hyper::body::to_bytes(req.into_body()).await?;
     let json_result: Result<Value, _> = serde_json::from_slice(&body_bytes);
+    let webhook_url = env::var("WEBHOOK_URL").expect("WEBHOOK NOT FOUND");
 
     if let Ok(json) = json_result {
         // Print the JSON message
         println!("Received JSON: {}", json);
 
+        send_discord_notification(&webhook_url, &json).await;
         // Send a response
         Ok(Response::new(Body::from("Message received and printed")))
     } else {
@@ -25,8 +33,10 @@ async fn handle_request(req: Request<Body>) -> Result<Response<Body>, Error> {
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
+
+    let port = env::var("PORT").as_f64().unwrap_or(9000);
     // Set the address to bind the server
-    let addr = SocketAddr::from(([127, 0, 0, 1], 9000));
+    let addr = SocketAddr::from(([127, 0, 0, 1], port));
 
     // Create a service function
     let make_svc = make_service_fn(|_conn| {
