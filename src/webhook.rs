fe6c68a8ecd851bce6ca809d28c14e4bfbca65b6
Flippchen <91947480+Flippchen@users.noreply.github.com>
2023-03-27 10:23:07 +0200

    added preprocessing of payload and new itle

--- src/webhook.rs
+++ src/webhook.rs
@@ -1,6 +1,6 @@
 use reqwest::Client;
 use serde::Serialize;
-
+use serde_json::Value;
 
 /// Represents the content of a Discord webhook message
 #[derive(Debug, Serialize)]
@@ -17,17 +17,35 @@ struct DiscordEmbed {
     color: u32,
 }
 
+/// Processes the payload of a webhook message
+fn process_payload(payload: &Value) -> String {
+    let accuracy = payload["accuracy"].as_f64().unwrap_or(0.0);
+    let loss = payload["loss"].as_f64().unwrap_or(0.0);
+    let val_accuracy = payload["val_accuracy"].as_f64().unwrap_or(0.0);
+    let val_loss = payload["val_loss"].as_f64().unwrap_or(0.0);
+
+    format!(
+        "Accuracy: {:.6}\nLoss: {:.6}\nValidation Accuracy: {:.6}\nValidation Loss: {:.6}",
+        accuracy,  loss, val_accuracy, val_loss
+    )
+}
+
 /// Sends a message to a Discord server using a webhook
-pub(crate) async fn send_discord_notification(webhook: &str, payload: &serde_json::Value) {
+pub(crate) async fn send_discord_notification(webhook: &str, payload: &Value) {
+
+    // Extract the epoch from payload
+    let epoch = payload["epoch"].as_i64().unwrap_or(0);
+    // Process the payload
+    let message: String = process_payload(payload);
 
     // Create a Discord embed with the given payload
     let embed = DiscordEmbed {
-        title: "Update Keras Model".to_string(),
-        description: payload.to_string(),
+        title: format!("Epoch: {}", epoch).to_string(),
+        description: message.to_string(),
         color: 0x000000,
     };
     let discord_message = DiscordMessage {
-        username: "Training".to_string(),
+        username: "Training Updates".to_string(),
         embeds: vec![embed],
     };
 
